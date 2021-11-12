771a032d45c24dccc43c09b790a4e741f7b55f0a
Casimiro de Almeida Barreto <casimiro.barreto@gmail.com>
2021-11-12 18:13:09 -0300

    2021-11-12 18:13

--- Tensorflow-Basic/pass_5.py
+++ Tensorflow-Basic/pass_5.py
@@ -35,13 +35,13 @@ print('X_test  shape is: ',X_test.shape)
 #model = tf.keras.Sequential()
 model = tf.keras.Sequential()
 # A convolutional layer 32x32x3 activation=ReLU, with padding
-model.add(tf.keras.layers.Conv2D(32, (3,3), padding='valid', activation='relu', input_shape=(32,32,3)))
-#model.add(tf.keras.layers.Conv2D(128, (3,3), padding='valid', activation='relu', input_shape=(32,32,3)))
+#model.add(tf.keras.layers.Conv2D(32, (3,3), padding='valid', activation='relu', input_shape=(32,32,3)))
+model.add(tf.keras.layers.Conv2D(128, (3,3), padding='valid', activation='relu', input_shape=(32,32,3)))
 #model.add(tf.keras.layers.Conv2D(256, (3,3), padding='valid', activation='relu', input_shape=(32,32,3)))
 # Do a maxpool 2x2 stride=2
 model.add(tf.keras.layers.MaxPooling2D((2,2), strides=2))
-model.add(tf.keras.layers.Conv2D(48, (3, 3), padding='valid', activation='relu'))
-#model.add(tf.keras.layers.Conv2D(192, (3, 3), padding='valid', activation='relu'))
+#model.add(tf.keras.layers.Conv2D(48, (3, 3), padding='valid', activation='relu'))
+model.add(tf.keras.layers.Conv2D(192, (3, 3), padding='valid', activation='relu'))
 #model.add(tf.keras.layers.Conv2D(384, (3, 3), padding='valid', activation='relu'))
 # Do a maxpool 2x2 stride=2
 model.add(tf.keras.layers.MaxPooling2D((2, 2), strides=2))
--- Tensorflow-GAN/display_image.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-#-*- coding: utf-8 -*-
-
-import PIL
-
-def display_image(image_index):
-    return PIL.Image.open('./images/image_at_epoch_{:04d}.png'.format(image_index))
